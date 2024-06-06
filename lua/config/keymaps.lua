4ad9d4c5e3a5f6edc905a9c952161db2e47378b1
wjchen <chenweijie@ledatech.cn>
2024-06-06 21:14:43 +0800

    nim v3: correct keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -21,3 +21,10 @@ for _, mapping in ipairs(nmappings) do
 end
 
 -- 需要silent
+vim.keymap.set('n', 'z<space>', function()    -- 在普通模式下，利用 z<space> 切换折叠
+  if vim.fn.foldclosed(vim.fn.line('.')) < 0 then   -- 判断当前行折叠是否关闭
+    vim.cmd('normal zc')                   -- 如果关闭，则折叠当前区域
+  else
+    vim.cmd('normal zo')                   -- 如果开启，则展开当前区域
+  end
+end, { noremap = true, silent = true })       -- 映射选项：不使用递归映射和静默执行
