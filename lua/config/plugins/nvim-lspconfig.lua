99f48dec43ab9833e1c857ae88389278f7e6febe
wjchen <chenweijie@ledatech.cn>
2024-06-12 14:36:18 +0800

    nvim v11: set lspconfig

--- lua/config/plugins/nvim-lspconfig.lua
+++ lua/config/plugins/nvim-lspconfig.lua
@@ -1,4 +1,22 @@
 return {
   'neovim/nvim-lspconfig',
-}
+  after = 'mason-lspconfig.nvim',  -- 确保在 mason-lspconfig 之后加载
+  config = function()
+    local lspconfig = require('lspconfig')
+    -- 定义通用的 on_attach 函数
+    local on_attach = function(client, bufnr)
+      -- 你可以在这里定义键位绑定，例如:
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true })
+    end
 
+    -- 定义 capabilities
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+    -- 配置每个必要的 LSP server
+    lspconfig.clangd.setup({ on_attach = on_attach, capabilities = capabilities })
+    lspconfig.pyright.setup({ on_attach = on_attach, capabilities = capabilities })
+    lspconfig.bashls.setup({ on_attach = on_attach, capabilities = capabilities })
+    lspconfig.lua_ls.setup({ on_attach = on_attach, capabilities = capabilities })
+    lspconfig.vimls.setup({ on_attach = on_attach, capabilities = capabilities })
+  end
+}
