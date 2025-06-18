51e826687fae2d00e4e6f4a6164212c4df51f05f
wjchen <chenweijie@ledatech.cn>
2025-06-18 21:15:54 +0800

    nvim v18: keymap: <F9> -> compile & run cpp file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -41,3 +41,27 @@ vim.keymap.set('n', 'z<space>', function()    -- 在普通模式下，利用 z<s
   end
 end, { noremap = true, silent = true })       -- 映射选项：不使用递归映射和静默执行
 
+-- 编译运行cpp文件
+local function compile_and_run_cpp()
+  -- 1. 保存当前文件
+  vim.cmd('w')
+
+  -- 2. 获取文件名，vim.fn.shellescape 是 Lua 中调用 shellescape 的方式
+  local source_file = vim.fn.shellescape(vim.fn.expand('%'))
+  local output_file = vim.fn.shellescape(vim.fn.expand('%<'))
+
+  -- 3. 构建命令字符串
+  local cmd = string.format('g++-14 -std=c++20 -Wall %s -o %s && ./%s', source_file, output_file, output_file)
+
+  -- 4. 在新的垂直分屏中打开终端并执行
+  vim.cmd('vsplit')
+  vim.cmd('terminal ' .. cmd)
+end
+
+-- 绑定到 F9
+vim.keymap.set('n', '<F9>', compile_and_run_cpp, {
+  noremap = true,
+  silent = true,
+  desc = "Compile and run C++ file"
+})
+
